1a6f457a6dd4af53602e35fd350cd0438fcbf565
victoralmeida1766 <victoralmeida1766@gmail.com>
2021-12-13 16:16:16 -0300

    Adicionando cadastro

---
+++ src/views/produtos/cadastro.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class CadastroProduto extends React.Component{
+
+    render(){
+        return(
+            <div className="card">
+                <div className="card-header"> Cadastro de produtos </div>
+                <div className="card-body"></div>
+            </div>
+        )
+    }
+}
+
+export default CadastroProduto;
